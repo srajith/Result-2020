1a1131434ac397c437a4b980c7c8353479214897
Rajith <srajith2001@gmail.com>
2020-03-25 15:05:13 +0530

    Added arrears.py

---
+++ arrears.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author: srajith
+# @Date:   2020-03-25 14:08:18
+# @Last Modified by:   srajith
+# @Last Modified time: 2020-03-25 15:04:13
+
+import requests
+import re
+import concurrent.futures
+
+
+def run(user):
+    username = user
+
+    data = {
+        '__LASTFOCUS': '',
+        '__VIEWSTATE': '/wEPDwULLTExNDk4NTI2NzcPZBYCAgMPZBYUAg0PD2QWAh4Hb25jbGljawURcmV0dXJuIHZhbGlkYXRlKClkAg8PDxYCHgdWaXNpYmxlaGRkAhMPDxYCHwFoZGQCFQ88KwAKAGQCFw88KwARAwAPZBYCHgtib3JkZXJjb2xvcgUHI0Y0RjRGNAEQFgAWABYADBQrAABkAhkPDxYCHwFoZGQCGw8PFgIfAWhkZAIdDw8WAh8BaGRkAh8PDxYCHwFoZGQCIQ8PFgIfAWhkZBgCBQlHcmlkVmlldzEPZ2QFCUZvcm1WaWV3MQ9nZLaGGV84hEKN39bx3qJj+4a+uqf4R6P0rR8C8lOVSVpq',
+        '__VIEWSTATEGENERATOR': 'A980467A',
+        '__EVENTTARGET': '',
+        '__EVENTARGUMENT': '',
+        '__EVENTVALIDATION': '/wEdAAMM1iTKfqRaR+qYZptN5JwpESCFkFW/RuhzY1oLb/NUVM34O/GfAV4V4n0wgFZHr3fON8hWKDQq3TURb4VWk91Q+JSmQ8P4fnfGKZMawLVg9Q==',
+        'TextBox1': username,  # '221810495',
+        'Button1': 'Get Marks'
+    }
+
+    response = requests.post(
+        'https://egovernance.unom.ac.in/resultnocap/', data=data)
+    re_status = re.findall(
+        r'<b>RA</b>|<b>A</b>|<b>F</b>|<b>P</b>', response.text)
+
+    re_sub = re.findall(r'<b>\w{5}</b>', response.text)
+    re_sub = re_sub[2:]
+    re_sub = re.findall(r'\w{5}', ''.join(re_sub))
+
+    string = ""
+    arrear = {}
+    for i in range(len(re_status)):
+        if "A" in re_status[i]:
+            string += re_sub[i] + ","
+    arrear[username] = string
+    print(arrear)
+
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    roll_number = [roll for roll in range(221810449, 221810499)]
+    executor.map(run, roll_number)
